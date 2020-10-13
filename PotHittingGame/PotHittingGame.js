5bdd1f3870fd5d298e0cfa5e0745559145565fb1
Kalina <kalinagg@yahoo.de>
2020-10-13 19:31:58 +0200

    Code improvements

--- PotHittingGame/PotHittingGame.js
+++ PotHittingGame/PotHittingGame.js
@@ -3,25 +3,31 @@ export default class PotHittingGame {
         this.pot = document.querySelector('.pot');
         this.hint = document.querySelector('.hint');
         this.playground = document.querySelector('.playground');
+        this.counter = document.querySelector('.counter');
+        this.success = document.querySelector('.success');
+        this.message = document.querySelector('.message');
         
         this.potWidth = this.pot.clientWidth;
         this.xPot = Math.floor(Math.random() * (500 - this.potWidth));
         this.yPot = Math.floor(Math.random() * (600 - this.potWidth));
         this.prevDistance = 0;
         this.count = 0;
+        this.isPotHit = false;
     }
 
     init() {
-        this.positionPot();        
+        this.positionPot();                
         this.playground.addEventListener('click', event => this.handleClicks(event));        
     }
 
     handleClicks(event) {
-        const xClick = this.getXClick(event);
-        const yClick = this.getYClick(event);
+        if (this.isPotHit) return;
+
+        const clickCoords = this.getClickCoordinates(event);
         this.count++;
         
-        if (this.isHit(xClick, yClick)) {
+        if (this.isHit(clickCoords)) {
+            this.isPotHit = true;
             this.showSuccessMessage();
             return;        
         }
@@ -29,17 +35,18 @@ export default class PotHittingGame {
         this.showClicks();
     
         if (this.count < 2) {
-            this.prevDistance = this.getDistanceToPot(xClick, yClick);
+            this.prevDistance = this.getDistanceToPot(clickCoords);
             return;
         }
 
-        const currDistance = this.getDistanceToPot(xClick, yClick);
+        const currDistance = this.getDistanceToPot(clickCoords);
 
-        this.showHintMessage(currDistance, xClick, yClick);
+        this.showHintMessage(currDistance, clickCoords);
     
-        if (this.isHit(xClick, yClick)) {
+        if (this.isHit(clickCoords)) {
+            this.isPotHit = true;
             this.showSuccessMessage();
-            return;        
+            return;  
         }
     
         this.prevDistance = currDistance;
@@ -48,50 +55,49 @@ export default class PotHittingGame {
     positionPot(pot) {
         this.pot.style.top = `${this.yPot}px`;
         this.pot.style.left = `${this.xPot}px`;
+    }    
+
+    getClickCoordinates(event) {
+        return {
+            x: event.clientX - Math.floor(this.playground.getBoundingClientRect().left),
+            y: event.clientY - Math.floor(this.playground.getBoundingClientRect().top)
+        }
     }
 
-    getXClick(event) {
-        return event.clientX - Math.floor(this.playground.getBoundingClientRect().left);
-    }
-    
-    getYClick(event) {
-        return event.clientY - Math.floor(this.playground.getBoundingClientRect().top);
-    }
-
-    getDistanceToPot(xClick, yClick) {
-        const xDist = this.xPot - xClick;
-        const yDist = this.yPot - yClick;
+    getDistanceToPot({x, y}) {
+        const xDist = this.xPot - x;
+        const yDist = this.yPot - y;
     
         return Math.floor(Math.sqrt((xDist * xDist) + (yDist * yDist)));
     }
 
-    isHit(xClick, yClick) {
-        const isHitX = xClick >= this.xPot && xClick <= this.xPot + this.potWidth;
-        const isHitY = yClick >= this.yPot && yClick <= this.yPot + this.potWidth;
+    isHit({x, y}) {
+        const isHitX = x >= this.xPot && x <= this.xPot + this.potWidth;
+        const isHitY = y >= this.yPot && y <= this.yPot + this.potWidth;
     
         return isHitX && isHitY;
     }
 
     showClicks() {        
-        document.querySelector('.counter').innerHTML = this.count;
+        this.counter.innerHTML = this.count;
     }
 
-    showHintMessage(currDistance, xClick, yClick) {
+    showHintMessage(currDistance, {x, y}) {
         if (this.prevDistance <= currDistance) {
             this.hint.innerHTML = '<strong>Cold!</strong>';
         } else {
             this.hint.innerHTML = '<strong>Hot!</strong>';
         }
 
-        this.hint.style.left = `${xClick + 20}px`;
-        this.hint.style.top = `${yClick + 20}px`;
+        this.hint.style.left = `${x + 20}px`;
+        this.hint.style.top = `${y + 20}px`;
     }
 
     showSuccessMessage() {
-        document.querySelector('.success').innerHTML = `You won the game with <strong>${this.count}</strong> clicks!`;
-        document.querySelector('.message').style.display = 'none';
-        this.hint.style.display = 'none';
+        this.success.innerHTML = `You won the game with <strong>${this.count}</strong> clicks!`;
+        this.message.style.display = 'none';
         this.playground.style.pointerEvents = 'none';
+        this.hint.style.display = 'none';
         this.pot.style.visibility = 'visible';
     }    
 }
